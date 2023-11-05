285837eb6a6ddef423c6a6c401cb7cc612235229
lukman-dev <lukmanulhakim.dev@gmail.com>
2023-11-06 02:32:59 +0700

    update say hellow woth param

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/lukman-dev/go-say-hello
+module github.com/lukman-dev/go-say-hello/v2
 
 go 1.21.3
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Say Hello Beow"
+func SayHello(name string) string {
+	return "Hallo " + name + "!"
 }
